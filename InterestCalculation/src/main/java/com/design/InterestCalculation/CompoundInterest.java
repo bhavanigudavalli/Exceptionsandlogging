316603507288fbfb56f5aa076526b518b295a9d8
bhavanigudavalli <39005965+bhavanigudavalli@users.noreply.github.com>
2020-02-26 23:38:17 +0530

    Add files via upload

---
+++ InterestCalculation/src/main/java/com/design/InterestCalculation/CompoundInterest.java
@@ -0,0 +1,29 @@
+package com.design.InterestCalculation;
+import java.io.*;
+import java.util.Scanner;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class CompoundInterest{
+public static final Logger logger = LogManager.getLogger(CompoundInterest.class);
+
+float principleAmount,rate,time,compoundInterest,n;
+void readValues() throws IOException{
+Scanner sc = new Scanner(System.in);
+logger.info("Enter Principle Amount : ");
+principleAmount = sc.nextFloat();
+logger.info("Enter Rate : ");
+rate = sc.nextFloat();
+logger.info("Enter Time Period : ");
+time = sc.nextFloat();
+}
+void calculateCompoundInterest(){
+compoundInterest = (float) (principleAmount * (Math.pow((1 + rate / 100.0), time)));
+}
+void displayAmount() throws IOException{
+logger.info("Total Amount : "+compoundInterest);
+}
+
+}
+
---
+++ InterestCalculation/src/main/java/com/design/InterestCalculation/SimpleInterest.java
@@ -0,0 +1,28 @@
+package com.design.InterestCalculation;
+import java.io.*;
+import java.util.Scanner;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class SimpleInterest{
+public static final Logger logger = LogManager.getLogger(SimpleInterest.class);
+float principleAmount,rate,time,simpleInterest;
+void readValues() throws IOException{
+@SuppressWarnings("resource")
+Scanner sc = new Scanner(System.in);
+logger.info("Enter Principle Amount : ");
+principleAmount = sc.nextFloat();
+logger.info("Enter Rate : ");
+rate = sc.nextFloat();
+logger.info("Enter Time Period : ");
+time = sc.nextFloat();
+}
+void calculateSimpleInterest(){
+simpleInterest = (principleAmount * rate * time)/100;
+}
+void displayAmount() throws IOException{
+logger.info("Total Amount : "+simpleInterest);
+  }
+}
